86a80b6bfa3b342b0c37ac038b15e2d457ec1ac9
yaxue1123 <yaxue@live.unc.edu>
2018-11-08 20:43:34 -0500

    move methods to class

--- script.js
+++ script.js
@@ -44,4 +44,83 @@ class Mine {
             return this.generateRandomLocation();
         }
     }
+
+    // Render. Draw the mine field.
+    // Draw the mine field with buttons.
+    drawMine(){
+        // Draw the mine using buttons.
+        // Set each button with id i-j to refer to the location in the 2d array. 
+        let mine = this.createMine();
+
+        for(let i = 1; i <= height; i++){
+            $("#minefield").append("<div class = 'mine'>");
+            for (let j = 1; j <= width; j++){
+                $("#minefield").append("<button x = " + i + " y = " + j + 
+                " hasBomb = " + mine[i][j] + " id = " + i + "-" + j + 
+                " clicked = 0" + ">" + mine[i][j] +"</button>");
+            }
+            $("#minefield").append("</div>");
+        }
+    };
+
+    // Calculate how many surrounding bombs.
+    // Input: int i and j. i ranges from [1, height], j ranges from [1, width].
+    countBomb(i, j) {
+        return this.mine[i-1][j-1] + this.mine[i-1][j] + this.mine[i-1][j+1] +
+                this.mine[i][j-1] + this.mine[i][j+1] +
+                this.mine[i+1][j-1] + this.mine[i+1][j] + this.mine[i+1][j+1];
+    }
+
+    // Input: the id of button.
+    clickButton(id) {
+        let cur_button = $("#"+id);
+
+        cur_button.css("background-color", "white");
+        cur_button.attr("clicked", 1);
+
+        if(parseInt(cur_button.attr("hasBomb")) === 1){
+            alert("Bomb!!!");
+            location.reload();
+        } else {
+            // See if surrounding has bomb.
+            let x = parseInt(cur_button.attr("x"));
+            let y = parseInt(cur_button.attr("y"));
+            let count = this.countBomb(x, y);
+            if (count !== 0) {
+                cur_button.html("<a class = 'bomb_count'>" + count + "</a>");
+            } else {
+                // If the current field has no bomb, then explore the adjacent ones (8 at max).
+                // Keep exploring until find bomb.
+                cur_button.text("");
+                // Traverse all adjacent field.
+                for (let i = x - 1; i <= x + 1; i++) {
+                    for (let j = y - 1; j <= y + 1; j++) {
+                        // Generate button id.
+                        let adj_id = i + "-" + j;
+                        // Fake click on unclicked ones iteratively.
+                        if ($("#"+adj_id).attr("clicked") === '0') {
+                            mine.clickButton(adj_id);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Getters.
+    getHeight() {
+        return this.height;
+    }
+
+    getWidth() {
+        return this.width;
+    }
+
+    getTotalBomb() {
+        return this.total_bomb;
+    }
+
+    getMine() {
+        return this.mine;
+    }
 }
